f95bb95be770d012b44e5d5ad9195d491df9213f
O1Dii <oldi730051@mail.ru>
2019-05-20 20:00:30 +0300

    side_menu improved

--- static/scripts/side_menu.js
+++ static/scripts/side_menu.js
@@ -1,9 +1,27 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+function openCloseNav() {
+  let leftMenu = document.getElementById("mySidenav");
+
+  if(leftMenu.style.width === "0px" || leftMenu.style.width === "") {
+    leftMenu.style.width = "30%";
+    document.getElementById("main").style.marginLeft = "15%";
+    leftMenu.style.paddingLeft = "10px";
+    leftMenu.style.paddingRight = "10px";
+  }
+  else{
+    leftMenu.style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    leftMenu.style.paddingLeft = "0";
+    leftMenu.style.paddingRight = "0";
+  }
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-}
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
