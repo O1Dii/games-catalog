989c6e1f8a22fed0aedbeb683c0e6ae7fb9cc5ec
O1Dii <oldi730051@mail.ru>
2019-05-22 20:37:03 +0300

    logo fixed, 2 sided slider added

--- static/scripts/side_menu.js
+++ static/scripts/side_menu.js
@@ -6,22 +6,43 @@ function openCloseNav() {
     document.getElementById("main").style.marginLeft = "15%";
     leftMenu.style.paddingLeft = "10px";
     leftMenu.style.paddingRight = "10px";
+    document.getElementById("filters-title").style.display = "block";
   }
   else{
     leftMenu.style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
     leftMenu.style.paddingLeft = "0";
     leftMenu.style.paddingRight = "0";
+    document.getElementById("filters-title").style.display = "none";
   }
 }
 
-var slider = document.getElementById('slider');
 
-noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 0,
-        'max': 100
-    }
-});
+
+function getVals(){
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if( slide1 > slide2 ){ let tmp = slide2; slide2 = slide1; slide1 = tmp; }
+
+  var displayElement = parent.getElementsByClassName("rangeValues")[0];
+      displayElement.innerHTML = slide1 + " - " + slide2;
+}
+
+window.onload = function(){
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName("range-slider");
+      for( var x = 0; x < sliderSections.length; x++ ){
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for( var y = 0; y < sliders.length; y++ ){
+          if( sliders[y].type ==="range" ){
+            sliders[y].oninput = getVals;
+            // Manually trigger event first time to display values
+            sliders[y].oninput();
+          }
+        }
+      }
+}
