5a4015a62ad5fb50401ad68554fdc78ee66d8efb
O1Dii <oldi730051@mail.ru>
2019-05-24 13:04:09 +0300

    fixed issues

--- main_app/igdb_api.py
+++ main_app/igdb_api.py
@@ -1,56 +1,60 @@
+import math
+
 import requests
-from math import ceil
-from itechart_project.settings import IGDB_API_KEY
+from urllib.parse import urlencode, quote_plus
+
+from django.conf import settings
 
 
 class IGDB:
-    __api_key = IGDB_API_KEY
-    __api_url = 'https://api-v3.igdb.com/'
-    __headers = {
-        'user-key': __api_key,
-        'Accept': 'application/json'
-    }
-
     def __init__(self):
         self.__last_headers_x_count = 1
+        self.__api_key = settings.IGDB_API_KEY
+        self.__api_url = 'https://api-v3.igdb.com/'
+        self.__headers = {
+            'user-key': self.__api_key,
+            'Accept': 'application/json'
+        }
+        self.__limit = 6
 
     def __api_get(self, additional_string: str = ''):
-        data = requests.get(self.__api_url + additional_string, headers=self.__headers)
-        x_count = int(data.headers.get('X-Count'))
+        print(f'{self.__api_url}{additional_string}')
+        data = requests.get(f'{self.__api_url}{additional_string}', headers=self.__headers)
+        try:
+            x_count = int(data.headers.get('X-Count'))
+        except TypeError:
+            x_count = 1
         if x_count > 1:
             self.__last_headers_x_count = x_count
         return data.json()
 
     def api_get_game(self, game_id):
         data = self.__api_get(f'games/{game_id}?fields=rating,version_title,aggregated_rating,'
-                              f' screenshots,summary,cover,platforms,genres')
-        print(data)
+                              f'screenshots,summary,cover,platforms,genres')
         return data
 
     def __generate_filters(self, search: str = None, genres: str = None,
-                           platforms: str = None, user_ratings: tuple = None) -> str:
-        result = []
-        if search is not None:
-            result.append(f'&search={search.replace(" ", "%20")}&')
-        if any((genres, platforms, user_ratings)):
-            result.append('filter')
-        if genres is not None:
-            result.append(f'[genres][eq]={genres},')
-        if platforms is not None:
-            result.append(f'[platforms][eq]={platforms},')
-        if user_ratings is not None:
+                           platforms: str = None, user_ratings: tuple = None) -> dict:
+        result = {}
+        filters = []
+        if search:
+            result['search'] = search
+        if genres:
+            filters.append(f'[genres][eq]={genres}')
+        if platforms:
+            filters.append(f'[platforms][eq]={platforms}')
+        if user_ratings:
             try:
-                result.append(f'[rating][gt]={user_ratings[0]},[rating][lt]={user_ratings[1]},')
+                filters.append(f'[rating][gt]={user_ratings[0] * 10},[rating][lt]={user_ratings[1] * 10}')
             except IndexError as e:
                 print(e)
-        if len(result) != 0:
-            return ''.join(result)[:-1]
-        else:
-            return ''
+        if filters:
+            result[''] = f'filter{",".join(filters)}'
+        return result
 
     def api_get_last_pages_amount(self) -> int:
         """returns total amount of pages for last game list"""
-        return ceil(self.__last_headers_x_count / 6)
+        return math.ceil(self.__last_headers_x_count / self.__limit)
 
     def api_get_games_list(self, page: int = 0, **kwargs) -> list:
         """kwargs:
@@ -58,15 +62,21 @@ class IGDB:
         genres: str = None,
         platforms: str = None,
         user_ratings: tuple = None"""
-        additional = f'games/?fields=name,cover,version_title&limit=6&offset={page * 6}'
+        category = 'games/?'
+        additional = {'fields': 'name,cover,version_title,rating',
+                      'limit': self.__limit,
+                      'offset': page * self.__limit}
         if kwargs.get('search') is None:
-            additional += '&order=popularity:desc'
+            additional['order'] = 'popularity:desc'
         filters = self.__generate_filters(**kwargs)
-        data = self.__api_get(additional + filters)
-        for i in range(len(data)):
-            if data[i].get('cover') is not None:
-                data[i]['cover'] = self.api_get_image(data[i].get('cover'), True)
-        print(data)
+        additional.update(filters)
+        data = self.__api_get(f'{category}{urlencode(additional, quote_via=quote_plus)}')
+        try:
+            for i, each in enumerate(data):
+                if each.get('cover'):
+                    data[i]['cover'] = self.api_get_image(each.get('cover'), True)
+        except AttributeError as e:
+            print(e)
         return data
 
     def api_get_image(self, image_id: int, cover=False) -> str:
