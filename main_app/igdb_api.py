fd649edf40153de85b6f7e51281959fa6e5e75d8
O1Dii <oldi730051@mail.ru>
2019-05-24 14:22:38 +0300

    refactored igdb_api

--- main_app/igdb_api.py
+++ main_app/igdb_api.py
@@ -1,36 +1,37 @@
 import math
+from urllib.parse import urlencode, quote_plus
 
 import requests
-from urllib.parse import urlencode, quote_plus
 
 from django.conf import settings
 
 
 class IGDB:
-    def __init__(self):
+    def __init__(self, limit):
         self.__last_headers_x_count = 1
-        self.__api_key = settings.IGDB_API_KEY
         self.__api_url = 'https://api-v3.igdb.com/'
         self.__headers = {
-            'user-key': self.__api_key,
+            'user-key': settings.IGDB_API_KEY,
             'Accept': 'application/json'
         }
-        self.__limit = 6
+        self.__limit = limit
 
     def __api_get(self, additional_string: str = ''):
         print(f'{self.__api_url}{additional_string}')
         data = requests.get(f'{self.__api_url}{additional_string}', headers=self.__headers)
-        try:
-            x_count = int(data.headers.get('X-Count'))
-        except TypeError:
-            x_count = 1
+        x_count = int(data.headers.get('X-Count', 1))
         if x_count > 1:
             self.__last_headers_x_count = x_count
         return data.json()
 
     def api_get_game(self, game_id):
-        data = self.__api_get(f'games/{game_id}?fields=rating,version_title,aggregated_rating,'
-                              f'screenshots,summary,cover,platforms,genres')
+        category = f'games/{game_id}?'
+        additional = {
+            'fields': 'rating,version_title,aggregated_rating,'
+                      'screenshots,summary,cover,platforms,genres'
+        }
+        encoded_url = urlencode(additional)
+        data = self.__api_get(f'{category}{encoded_url}')
         return data
 
     def __generate_filters(self, search: str = None, genres: str = None,
@@ -44,10 +45,13 @@ class IGDB:
         if platforms:
             filters.append(f'[platforms][eq]={platforms}')
         if user_ratings:
-            try:
-                filters.append(f'[rating][gt]={user_ratings[0] * 10},[rating][lt]={user_ratings[1] * 10}')
-            except IndexError as e:
-                print(e)
+            filters.append('[rating]')
+            if user_ratings[0]:
+                filters[-1] += f'[gt]={user_ratings[0] * 10}'
+            if user_ratings[0] and user_ratings[1]:
+                filters[-1] += ','
+            if user_ratings[1]:
+                filters[-1] += f'[lt]={user_ratings[1] * 10}'
         if filters:
             result[''] = f'filter{",".join(filters)}'
         return result
@@ -63,23 +67,24 @@ class IGDB:
         platforms: str = None,
         user_ratings: tuple = None"""
         category = 'games/?'
-        additional = {'fields': 'name,cover,version_title,rating',
-                      'limit': self.__limit,
-                      'offset': page * self.__limit}
+        additional = {
+            'fields': 'name,cover,version_title,rating',
+            'limit': self.__limit,
+            'offset': page * self.__limit}
         if kwargs.get('search') is None:
             additional['order'] = 'popularity:desc'
         filters = self.__generate_filters(**kwargs)
         additional.update(filters)
-        data = self.__api_get(f'{category}{urlencode(additional, quote_via=quote_plus)}')
-        try:
-            for i, each in enumerate(data):
+        encoded_url = urlencode(additional, quote_via=quote_plus)
+        data = self.__api_get(f'{category}{encoded_url}')
+        for i, each in enumerate(data):
+            if each:
                 if each.get('cover'):
                     data[i]['cover'] = self.api_get_image(each.get('cover'), True)
-        except AttributeError as e:
-            print(e)
         return data
 
     def api_get_image(self, image_id: int, cover=False) -> str:
         result = 'covers/' if cover else 'screenshots/'
         data = self.__api_get(result + str(image_id) + '?fields=url')
-        return 'https:' + data[0].get('url').replace('t_thumb', 't_logo_med' if cover else 't_screenshot_med')
+        return 'https:' + data[0].get('url').replace('t_thumb', 't_logo_med' if cover
+                                                     else 't_screenshot_med')
