e554b94f26fdbd1a6ec1c27cbab9075434109b7a
O1Dii <oldi730051@mail.ru>
2019-06-01 22:06:38 +0300

    Added postgresql

--- itechart_project/settings.py
+++ itechart_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'itechart_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'itechart_project',
+        'USER': 'itechart_project',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
